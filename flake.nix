f539eaa5cbb0f2955230e1ade9ee7551a73d92b7
Dataram57 <contact@dataram57.com>
2025-12-29 20:05:53 +0100

    eghh

--- flake.nix
+++ flake.nix
@@ -1,52 +1,46 @@
 {
-  description = "git-mirror_tracker CLI tool";
+    description = "git-mirror_tracker";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/master";
+    };
 
-  outputs = { self, nixpkgs }: 
-  let
-    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in
-  {
-    packages = forAllSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        git-mirror_tracker = pkgs.stdenv.mkDerivation {
-          pname = "git-mirror_tracker";
-          version = "1.0";
+    outputs = { self, nixpkgs }:
+    let
+        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
-          # Script as the source
-          src = ./git-mirror_tracker;
+        forAllSystems = nixpkgs.lib.genAttrs systems;
 
-          # Ensure git is available
-          buildInputs = [ pkgs.git ];
+    in {
+        packages = forAllSystems (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            git-mirror_tracker = pkgs.writeShellApplication {
+                name = "git-mirror_tracker";
 
-          # Install the script as a command
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/git-mirror_tracker
-            chmod +x $out/bin/git-mirror_tracker
-          '';
+                # Runtime dependencies
+                runtimeInputs = [
+                    pkgs.git
+                ];
 
-          meta = with pkgs.lib; {
-            description = "CLI tool to track git mirrors";
-            license = licenses.mit;
-            maintainers = [ maintainers.example ];
-          };
+                # The script contents
+                text = builtins.readFile ./git-mirror_tracker;
+
+                checkPhase = null;
+            };
+
+            default = self.packages.${system}.git-mirror_tracker;
+        }
+        );
+
+        apps = forAllSystems (system: {
+        git-mirror_tracker = {
+            type = "app";
+            program = "${self.packages.${system}.git-mirror_tracker}/bin/git-mirror_tracker";
         };
-      }
-    );
 
-    apps = forAllSystems (system: {
-      git-mirror_tracker = {
-        type = "app";
-        program = "${self.packages.${system}.git-mirror_tracker}/bin/git-mirror_tracker";
-      };
-      default = self.apps.${system}.git-mirror_tracker;
-    });
-  };
+        default = self.apps.${system}.git-mirror_tracker;
+        });
+    };
 }
