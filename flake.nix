5801bdd7138d5905be73d8452ab153c38c34b1c2
Dataram57 <contact@dataram57.com>
2025-12-29 19:37:15 +0100

    flake test + comments

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+    description = "git-mirror_tracker";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }:
+    let
+        systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+        forAllSystems = nixpkgs.lib.genAttrs systems;
+
+    in {
+        packages = forAllSystems (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            git-mirror_tracker = pkgs.writeShellApplication {
+                name = "git-mirror_tracker";
+
+                # Runtime dependencies
+                runtimeInputs = [
+                    pkgs.git
+                ];
+
+                # The script contents
+                text = builtins.readFile ./git-mirror_tracker;
+            };
+
+            default = self.packages.${system}.git-mirror_tracker;
+        }
+        );
+
+        apps = forAllSystems (system: {
+        git-mirror_tracker = {
+            type = "app";
+            program = "${self.packages.${system}.git-mirror_tracker}/bin/git-mirror_tracker";
+        };
+
+        default = self.apps.${system}.git-mirror_tracker;
+        });
+    };
+}
